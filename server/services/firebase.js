c853d31744e94df5f0c9cc10f4020b649ccf1eef
brenmurrell <bren@moon.co.nz>
2020-12-20 17:25:12 +1300

    remove unuesed credentials

--- server/services/firebase.js
+++ server/services/firebase.js
@@ -4,13 +4,6 @@ var serviceAccount = require('../../../dotvotes-39ccb-firebase-adminsdk-xt35c-6a
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
-  // credential: admin.credential.cert({
-  //   project_id: process.env.FB_PROJECT_ID,
-  //   private_key: process.env.FIREBASE_PRIVATE_KEY,
-  //   client_email: process.env.FIREBASE_CLIENT_EMAIL
-  // })
-  // credential: admin.credential.applicationDefault()
-  // credential: admin.credential.cert()
 })
 
 module.exports = admin
